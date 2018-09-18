4aadaf78908fab85a894012bddd06711190f5b3c
Somefriik <vytautas.stulpinas@gmail.com>
2018-09-18 21:05:09 +0300

    pirma complete

--- testinis/Program.cs
+++ testinis/Program.cs
@@ -8,8 +8,31 @@ namespace testinis
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int psk = Max(10, 20, 5);
+            Console.WriteLine(psk);
+            Console.ReadKey();
+
+
         }
+        static int Max(int psk, int ask, int tsk)
+        {
+            if (psk >= ask && psk >= tsk)
+            {
+                return psk;
+            }
+            else if (ask >= tsk)
+            {
+                return ask;
+            }
+            else
+            {
+                return tsk;
+            }
+
+        }
+
     }
+
 }
