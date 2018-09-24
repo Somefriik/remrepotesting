68d727058284abefc317c010741dcca36d2d878a
Somefriik <vytautas.stulpinas@gmail.com>
2018-09-24 19:50:31 +0300

    3+4...nevisai

--- testinis/Program.cs
+++ testinis/Program.cs
@@ -10,6 +10,43 @@ namespace testinis
     {
         static void Main(string[] args)
         {
+            //int pirm = Convert.ToInt32(Console.ReadLine());
+            int Di=0;
+
+           /* Console.WriteLine(IsPrimal(13));
+            Console.WriteLine(IsPrimal(12));*/
+            Console.WriteLine(D20(Di));
+            Console.ReadKey();
+
         }
+       private static bool IsPrimal(int pirm)
+        {
+           
+            for(int i=2;i<pirm;i++)
+            {
+                if (pirm % i == 0) {
+                    return false;
+                }
+               
+            }
+            return true;
+
+        }
+        private static int D20(int Di)
+        {
+
+            for (int DS = 0;)
+            {
+                if (Di > 0 && Di < 20)
+                {
+                    Console.WriteLine(Di);
+                   
+                }
+                
+                break;
+            }
+            return Di;
+        }
+
     }
 }
